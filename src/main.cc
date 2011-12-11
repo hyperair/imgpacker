baaff541d98d6bb9806d79dd3d0d57d36c2cb160
Chow Loong Jin <hyperair@ubuntu.com>
2011-12-11 17:39:22 +0800

    Catch stray exceptions in main()

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,17 @@
 #include <gtkmm.h>
+#include <iostream>
 #include <imgpack/application.hh>
 
 int main (int argc, char **argv)
 {
-    ImgPack::Application (argc, argv).run ();
+    try {
+        ImgPack::Application (argc, argv).run ();
+
+    } catch (std::exception &e) {
+        std::cerr << "Uncaught exception in main(). Terminating with exception"
+                  << e.what () << std::endl;
+        return 1;
+    }
 
     return 0;
 }
