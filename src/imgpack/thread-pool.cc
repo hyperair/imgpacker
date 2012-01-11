6d2da70ebdcfac7e7f55662423371c2abb07b419
Chow Loong Jin <hyperair@debian.org>
2012-01-11 18:02:21 +0800

    Add ThreadPool::hardware_concurrency()

--- src/imgpack/thread-pool.cc
+++ src/imgpack/thread-pool.cc
@@ -5,10 +5,16 @@
 using ImgPack::ThreadPool;
 
 ThreadPool::ThreadPool () :
-    Glib::ThreadPool (sysconf (_SC_NPROCESSORS_ONLN))
+    Glib::ThreadPool (hardware_concurrency ())
 {
     LOG(info) << "Initialized thread pool with max threads: "
               << get_max_threads ();
 }
 
-ThreadPool::~ThreadPool () {}
+ThreadPool::~ThreadPool (){}
+
+// static
+long ThreadPool::hardware_concurrency ()
+{
+    return sysconf (_SC_NPROCESSORS_ONLN);
+}
