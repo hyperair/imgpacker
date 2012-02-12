4f0a84eace009ff54a43373838830df0f194ca46
Chow Loong Jin <hyperair@debian.org>
2012-02-12 18:11:00 +0800

    Add CollageTreeView stub
    This will be a class that displays the actual layout tree and allow for
    modifying it by drag-dropping rectangles around in a directory tree-like structure.

---
+++ src/imgpack/gtkui/collage-treeview.cc
@@ -0,0 +1,12 @@
+#include <imgpack/gtkui/collage-treeview.hh>
+
+namespace ip = ImgPack;
+namespace ipg = ImgPack::GtkUI;
+
+using ipg::CollageTreeView;
+
+CollageTreeView::CollageTreeView ()
+{
+}
+
+CollageTreeView::~CollageTreeView () {}
